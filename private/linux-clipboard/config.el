5247abb58000ca249e50a4d925257630186accd2
jigyakkuma <jigyakkuma@gmail.com>
2017-08-31 11:18:26 +0900

    set global key binding

--- private/linux-clipboard/config.el
+++
@@ -1,2 +0,0 @@
-(evil-leader/set-key "o y" 'copy-to-clipboard)
-(evil-leader/set-key "o p" 'paste-from-clipboard)
---
+++ private/linux-clipboard/keybindings.el
@@ -0,0 +1,4 @@
+(spacemacs/declare-prefix "o" "clipboard")
+(spacemacs/set-leader-keys
+  "oy" 'copy-to-clipboard
+  "op" 'paste-from-clipboard)
