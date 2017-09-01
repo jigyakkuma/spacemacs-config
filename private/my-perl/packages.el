85820f7c35ee3cd4deaaaef9197a1da8e4b7e8d9
jigyakkuma <jigyakkuma@gmail.com>
2017-09-01 09:28:05 +0900

    一旦これで行く

--- private/my-perl/packages.el
+++ private/my-perl/packages.el
@@ -1,16 +1,18 @@
-(defconst my-perl-packages
-  '(
-    (cperl-mode :location elpa)
-    ))
+(setq my-perl-packages
+      '(
+        (cperl-mode :location elpa)
+        flycheck
+        ))
 
 (defun my-perl/init-cperl-mode ()
   (use-package cperl-mode
     :defer t
-    :init
-    (evil-leader/set-key-for-mode 'cperl-mode
-      "fr" 'perltidy-region
-      "fd" 'perltidy-defun
-      "fb" 'perltidy-buffer)
-    ))
+    :init (dolist (mode '(cperl-mode))
+            (spacemacs/declare-prefix "mf" "Perl/perltidy")
+            (spacemacs/set-leader-keys-for-major-mode mode
+              "fr" 'perltidy-region
+              "fd" 'perltidy-defun
+              "fb" 'perltidy-buffer))))
 
-;;; packages.el ends here
+(defun my-perl/post-init-flycheck ()
+  (spacemacs/add-flycheck-hook 'cperl-mode))
