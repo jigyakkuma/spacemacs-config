9d6a790447dc7d95e64e74145abc85f290e642b1
jigyakkuma <jigyakkuma@gmail.com>
2018-03-01 21:55:45 +0900

    Deleted header comment

--- private/my-perl/funcs.el
+++ private/my-perl/funcs.el
@@ -1,15 +1,3 @@
-;;; funcs.el --- Perl5 Layer functions File for Spacemacs
-;;
-;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
-;;
-;; Author: Eivind Fonn <evfonn@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
-
 (defun spacemacs//perl5-smartparens-enable ()
   (define-key cperl-mode-map "{" nil))
 
